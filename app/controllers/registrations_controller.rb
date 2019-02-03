f954f35f33a540aa5b925ff0a8be48733eae68a1
briansu <brianbb.su@gmail.com>
2019-02-03 09:48:49 +0000

    Disable public registration on production judge

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,9 +11,13 @@ class RegistrationsController < Devise::RegistrationsController
   end
     
   def create
-    super
-    resource.remote_avatar_url = "http://avatar.3sd.me/100"
-    resource.save
+    if Rails.env.production?
+      redirect_to root_path, alert: 'Registration is not allowed!'
+    else
+      super
+      resource.remote_avatar_url = "http://avatar.3sd.me/100"
+      resource.save
+    end
   end
 
   def update
