f210d4ad5f4d49b748d0edd48cd91f7743a8a225
adrien1018 <adrien1018@users.noreply.github.com>
2019-11-06 10:52:39 +0000

    Update compiler list

--- app/helpers/compiler_helper.rb
+++ app/helpers/compiler_helper.rb
@@ -1,14 +1,14 @@
 module CompilerHelper
   COMPILER_LIST = [
-    ["language-cpp", "c++14/gnu c++ compiler 9.1.0 | options: -O2 -std=c++14", "c++14"],
-    ["language-cpp", "c++17/gnu c++ compiler 9.1.0 | options: -O2 -std=c++17", "c++17"],
-    ["language-cpp", "c++11/gnu c++ compiler 9.1.0 | options: -O2 -std=c++11", "c++11"],
-    ["language-cpp", "c++98/gnu c++ compiler 9.1.0 | options: -O2 -std=c++98", "c++98"],
-    ["language-c", "c11/gnu c compiler 9.1.0 | options: -O2 -std=c11 -lm", "c11"],
-    ["language-c", "c99/gnu c compiler 9.1.0 | options: -O2 -std=c99 -lm", "c99"],
-    ["language-c", "c90/gnu c compiler 9.1.0 | options: -O2 -ansi -lm", "c90"],
+    ["language-cpp", "c++14/gnu c++ compiler 9.2.1 | options: -O2 -std=c++14", "c++14"],
+    ["language-cpp", "c++17/gnu c++ compiler 9.2.1 | options: -O2 -std=c++17", "c++17"],
+    ["language-cpp", "c++11/gnu c++ compiler 9.2.1 | options: -O2 -std=c++11", "c++11"],
+    ["language-cpp", "c++98/gnu c++ compiler 9.2.1 | options: -O2 -std=c++98", "c++98"],
+    ["language-c", "c11/gnu c compiler 9.2.1 | options: -O2 -std=c11 -lm", "c11"],
+    ["language-c", "c99/gnu c compiler 9.2.1 | options: -O2 -std=c99 -lm", "c99"],
+    ["language-c", "c90/gnu c compiler 9.2.1 | options: -O2 -ansi -lm", "c90"],
     ["language-python", "python2/CPython 2.7.12 | options: -m py_compile", "python2"],
-    ["language-python", "python3/CPython 3.7.3 | options: -m py_compile", "python3"],
+    ["language-python", "python3/CPython 3.7.5 | options: -m py_compile", "python3"],
     ["language-haskell", "haskell/glasgow haskell compiler 7.10.3 with haskell platform | options: -O", "haskell"],
   ]
 
