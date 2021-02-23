880c998cc259cdaeb2a90a41c83538a76f542e96
edisonhello <edisonhello@hotmail.com>
2021-02-23 08:33:29 +0000

    First version setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -x
+
+TIOJ_PATH=$HOME/tioj
+MIKU_PATH=$HOME/miku
+DB_USERNAME=root
+DB_PASSWORD=SAMPLE_PASSWORD
+URL=http://localhost
+
 ubuntu_distribution=`cat /etc/lsb-release | grep "RELEASE" | awk -F= '{ print $2 }'`
 
 if [[ $ubuntu_distribution == "16.04" ]] then
@@ -11,9 +19,9 @@ sudo apt-add-repository -y ppa:rael-gc/rvm
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.11-1_all.deb -O /tmp/mac.deb
 
 if [[ $ubuntu_distribution == "20.04" ]] then
-	echo "Choose ubuntu bionic for alternative in following interactive window."
+	echo "Choose ubuntu bionic for alternative in next interactive window."
 fi
-echo "Please choose mysql 5.7 in following interactive window."
+echo "Please choose mysql 5.7 in next interactive window."
 read -n 1 -p "Press any key to continue."
 sudo dpkg -i /tmp/mac.deb # Prepare to install MySQL
 
@@ -41,3 +49,68 @@ rvm use --default 2.6.5
 gem install rails -v 4.2.11
 
 newgrp $old_group
+
+git clone https://github.com/TIOJ-INFOR-Online-Judge/tioj $TIOJ_PATH
+git clone https://github.com/TIOJ-INFOR-Online-Judge/miku $MIKU_PATH
+git submodule update --init -C $MIKU_PATH
+
+cd $TIOJ_PATH
+bundle install
+
+echo "Please choose ruby as main programming language and use default settings in next interactive window."
+read -n 1 -p "Press any key to continue."
+sudo rvmsudo passenger-install-nginx-module
+
+sudo sed -i '/http {/a \ \ \ \ passenger_app_env production;' /opt/nginx/conf/nginx.conf
+sudo sed -i "/^[^#]*server_name/a \ \ \ \ \ \ \ \ passenger_enabled on;\n\ \ \ \ \ \ \ \ root $TIOJ_PATH/public;" /opt/nginx/conf/nginx.conf
+
+cat <<EOF > config/database.yml
+default: &default
+  adapter: mysql2
+  encoding: utf8
+  pool: 5
+  username: $DB_USERNAME
+  password: $DB_PASSWORD
+  socket: /var/run/mysqld/mysqld.sock
+
+development:
+  <<: *default
+    database: tioj_development
+
+test:
+  <<: *default
+   database: tioj_test
+
+production:
+  <<: *default
+   database: tioj_production
+EOF
+
+
+TOKEN=$(rake secret)
+KEY=$(rake secret)
+
+cd $TIOJ_PATH
+cat <<EOF > config/initializers/secret_token.rb
+Tioj::Application.config.secret_token = "$TOKEN"
+EOF
+
+cat <<EOF > config/initializers/fetch_key.rb
+Tioj::Application.config.fetch_key = "$KEY"
+EOF
+
+cd $MIKU_PATH
+cat <<EOF > app/tioj_url.py
+tioj_url = "$URL"
+tioj_key = "$KEY"
+EOF
+
+cd $TIOJ_PATH
+RAILS_ENV=production rake db:create db:schema:load db:seed
+RAILS_ENV=production rake assets:precompile
+mkdir public/announcement
+echo -n '{"name":"","message":""}' > public/announcement/anno
+
+cd $MIKU_PATH
+make -j
+
