1841ae5849629434fb496075c91776353fe38e61
edisonhello <edisonhello@hotmail.com>
2021-02-25 12:07:16 +0000

    Set password for ubuntu 20.04

--- setup.sh
+++ setup.sh
@@ -10,11 +10,6 @@ ubuntu_distribution=`cat /etc/lsb-release | grep "RELEASE" | awk -F= '{ print $2
 
 set -x
 
-if [[ $ubuntu_distribution == "16.04" ]]; then
-	sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-	sudo add-apt-repository ppa:carsten-uppenbrink-net/openssl -y
-fi
-
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.11-1_all.deb -O /tmp/mac.deb
 
 if [[ $ubuntu_distribution == "20.04" ]]; then
@@ -24,18 +19,21 @@ echo "Please choose mysql 5.7 in next interactive window."
 read -n 1 -p "Press any key to continue."
 sudo dpkg -i /tmp/mac.deb # Prepare to install MySQL
 
-sudo add-apt-repository ppa:rael-gc/rvm -y 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+if [[ $ubuntu_distribution == "16.04" ]]; then
+	sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y -n
+	sudo add-apt-repository ppa:carsten-uppenbrink-net/openssl -y -n
+fi
+sudo add-apt-repository ppa:rael-gc/rvm -y -n
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y -n
 sudo apt update --allow-unauthenticated
-sudo apt install gcc-9 g++-9 python python3 ghc rvm imagemagick mysql-server libmysqlclient-dev libcurl4-openssl-dev openssl libcap-dev -y
+sudo apt install gcc-9 g++-9 python python3 ghc rvm imagemagick mysql-server libmysqlclient-dev libcurl4-openssl-dev libcap-dev openssl -y
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 \
     --slave /usr/bin/g++ g++ /usr/bin/g++-9 \
     --slave /usr/bin/gcc-nm gcc-nm /usr/bin/gcc-nm-9 \
     --slave /usr/bin/gcc-ar gcc-ar /usr/bin/gcc-ar-9 \
     --slave /usr/bin/gcc-ranlib gcc-ranlib /usr/bin/gcc-ranlib-9 # Use GCC 9 as default
 
-# sudo mysql_secure_installation # Setup MySQL
-if [[ $ubuntu_distribution == "18.04" ]]; then
+if [[ $ubuntu_distribution != "16.04" ]]; then
     sudo mysql <<< "ALTER USER '$DB_USERNAME'@'localhost' IDENTIFIED WITH mysql_native_password BY '$DB_PASSWORD'; flush privileges;"
 fi
 sudo usermod -a -G rvm $USER
