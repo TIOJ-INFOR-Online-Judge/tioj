18aab66014ff66bdafe28d7edd7c73c53ac06583
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-01-24 21:55:05 +0800

    Hide problems before logging in

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,4 +1,5 @@
 class ProblemsController < ApplicationController
+  before_action :authenticate_user!
   before_action :authenticate_admin!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_problem, only: [:show, :edit, :update, :destroy, :ranklist]
   before_action :set_contest, only: [:show]
