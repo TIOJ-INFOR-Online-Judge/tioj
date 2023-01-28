f3d076fb51adda7e9515e7d72f5d7de326b7c827
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-01-29 00:54:45 +0800

    Use another environment variable instead of RAILS_ENV to disable register

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,12 +9,12 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def create
-    if Rails.env.production?
-      redirect_to root_path, alert: 'Registration is not allowed!'
-    else
+    if ENV["ALLOW_REGISTER"] == 'allow'
       super
       resource.remote_avatar_url = "http://avatar.3sd.me/100"
       resource.save
+    else
+      redirect_to root_path, alert: 'Registration is not allowed!'
     end
   end
 
