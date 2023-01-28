59e92a0d4fe8cf1112e22c3cfcc15b81f0ce848f
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-01-28 22:03:18 +0800

    Remove debug codes

--- app/assets/javascripts/ioicamp_contest_switch.js
+++ app/assets/javascripts/ioicamp_contest_switch.js
@@ -1,29 +1,16 @@
 function ioicamp_contest_dashboard_main(){
 
-	let _debug = false
-
-	function debug(...args) {
-		if (_debug === false) return
-		console.log(...args)
-	}
-
-	debug("ioicamp_contest_dashboard_refresh.js is loaded.")
 	let SWITCH = document.getElementById('display_switch')
 	let TABLE = document.querySelector("table.dashboard-table")
 
-	debug(SWITCH);
-
 	SWITCH.onchange = function() {
 		enable = SWITCH.parentNode.getAttribute('class').includes('switch-on');
-		debug(`SWITCH.onchange(), get enable = ${enable}`)
-
 		update(enable);
 	};
 
 	update(true);
 
 	function update(state) {
-		console.log(TABLE);
 		if (!state) {
 			TABLE.classList.add('dashboard-show-penalty');
 			TABLE.classList.remove('dashboard-show-score');
