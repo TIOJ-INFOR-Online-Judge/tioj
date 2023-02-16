ec2dda4150cd24b6d8453a7265225e2150c5c063
adrien1018 <adrien1018@users.noreply.github.com>
2023-02-16 18:10:29 +0800

    Suppress format detection on user page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   scope 'admin' do
-    resources :users, controller: 'admin/users', as: 'admin_users', constraints: { id: /[^\/]+/ }
+    resources :users, controller: 'admin/users', as: 'admin_users', constraints: { id: /[^\/]+/ }, defaults: { format: :html }, format: false
   end
   ActiveAdmin.routes(self)
 
@@ -37,7 +37,6 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users, constraints: { id: /[^\/]+/ }
   resources :posts do
     resources :comments, except: [:index]
   end
@@ -60,7 +59,7 @@ Rails.application.routes.draw do
   resources :articles
   resources :testdata, only: [:show]
 
-  resources :users do
+  resources :users, constraints: { id: /[^\/]+/ }, defaults: { format: :html }, format: false do
     member do
       get 'changed_problems'
       get 'changed_submissions'
@@ -87,55 +86,6 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
