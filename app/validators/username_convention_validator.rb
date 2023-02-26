082cc1d8acccc6e91107f19dd129838dfae48f4b
adrien1018 <adrien1018@users.noreply.github.com>
2023-02-26 14:04:02 +0000

    Fix username validation

--- app/validators/username_convention_validator.rb
+++ app/validators/username_convention_validator.rb
@@ -1,9 +1,10 @@
 class UsernameConventionValidator < ActiveModel::EachValidator
   def validate_each(record, field, value)
     unless value.blank?
-      record.errors[field] << "is not alphanumeric (letters, numbers, underscores or periods)" unless value =~ /^[[:alnum:]._-]+$/
-      record.errors[field] << "should start with a letter" unless value[0] =~ /[A-Za-z]/
-      record.errors[field] << "contains illegal characters" unless value.ascii_only?
+      record.errors.add field, "is not alphanumeric (letters, numbers, underscores or periods)" unless value =~ /^[[:alnum:]._-]+$/
+      record.errors.add field, "should start with a letter" unless value[0] =~ /[A-Za-z]/
+      record.errors.add field, "contains illegal characters" unless value.ascii_only?
+      record.errors.add field, "is forbidden" if ['sign_in', 'sign_up'].include? value
     end
   end
 end
