06c039065c43ddb8cc67c33801beadc453e66c6e
adrien1018 <adrien1018@users.noreply.github.com>
2023-03-06 16:47:51 +0000

    Fix username validation again

--- app/validators/username_convention_validator.rb
+++ app/validators/username_convention_validator.rb
@@ -7,6 +7,7 @@ class UsernameConventionValidator < ActiveModel::EachValidator
         record.errors.add field, "contains illegal characters" unless value.ascii_only?
       end
       record.errors.add field, "is forbidden" if ['sign_in', 'sign_up'].include? value
+      record.errors.add field, "should not be purely numeric" if value =~ /^[0-9]+$/
     end
   end
 end
