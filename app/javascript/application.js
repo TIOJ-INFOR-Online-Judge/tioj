4035bd126e285ae9137fb3d91fd1b266d5909d89
adrien1018 <adrien1018@users.noreply.github.com>
2023-04-18 12:00:52 +0000

    Fix sample testdata copy

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,6 +7,7 @@ import {createPopper} from '@popperjs/core';
 import 'bootstrap';
 import 'bootstrap-select';
 import './vendor/bootstrap-switch'
+import 'bootstrap-notify';
 
 // Flat UI
 import './vendor/flatui-checkbox';
--- app/javascript/helpers/init_code_copy_script.js
+++ app/javascript/helpers/init_code_copy_script.js
@@ -1,93 +1,56 @@
-(() => {
-	document.addEventListener("DOMContentLoaded", function () {
+$(() => {
+	$('.copy-group').each(function() {
+		let btn = $(this).find('.copy-group-btn').first();
+		let code = $(this).find('.copy-group-code').first();
 
-		let divs = document.querySelectorAll(".copy-group")
-
-		divs.forEach((div, idx, _) => initialize(div))
-
-		function initialize(div) {
-			let btn = div.getElementsByClassName("copy-group-btn")[0]
-			let code = div.getElementsByClassName("copy-group-code")[0]
-
-			btn.addEventListener('click', (e) => {
+		btn.on('click', () => {
+			let text = code.text();
+			if (navigator.clipboard) {
+				navigator.clipboard.writeText(text).then(generateSuccessNotify).catch(generateFailedNotify);
+			} else { // old deprecated method
 				try {
-					let el = document.createElement('textarea')
-					el.value = code.innerText
-					document.body.appendChild(el)
-					el.select()
-					if (document.execCommand('copy')) 
-						generate_success_notify()
-					else 
-						generate_failed_notify()
-					document.body.removeChild(el)
+					let tmp = $('<textarea>');
+					$('body').append(tmp);
+					tmp.val(text).trigger('select');
+					if (document.execCommand('copy')) {
+						generateSuccessNotify();
+					} else {
+						generateFailedNotify();
+					}
+					tmp.remove();
 				} catch (err) {
-					generate_error_notify(err)
+					generateNotify(err, 'danger');
 				}
-			})
-		}
-
-		function generate_success_notify() {
-			$.notify({
-				title: '<strong>Geez</strong>',
-				message: "Successful copied!"
-			}, {
-				type: 'success',
-				animate: {
-					enter: 'animated fadeInLeft animate_speed_0_4s',
-					exit: 'animated fadeOutRight'
-				},
-				placement: {
-					from: "bottom",
-					align: "right"
-				},
-				offset: 20,
-				spacing: 10,
-				z_index: 1031,
-				newest_on_top: true,
-			})
-		}
-
-		function generate_failed_notify() {
-			$.notify({
-				title: '<strong>Geez</strong>',
-				message: "Copy failed!"
-			},{
-				type: 'danger',
-				animate: {
-					enter: 'animated fadeInLeft',
-					exit: 'animated fadeOutRight'
-				},
-				placement: {
-					from: "bottom",
-					align: "right"
-				},
-				offset: 20,
-				spacing: 10,
-				z_index: 1031,
-				newest_on_top: true,
-			});
-		}
-
-		function generate_error_notify(err) {
-			$.notify({
-				title: '<strong>Geez</strong>',
-				message: err
-			},{
-				type: 'danger',
-				animate: {
-					enter: 'animated fadeInLeft',
-					exit: 'animated fadeOutRight'
-				},
-				placement: {
-					from: "bottom",
-					align: "right"
-				},
-				offset: 20,
-				spacing: 10,
-				z_index: 1031,
-				newest_on_top: true,
-			});
-		}
-
+			}
+		});
 	});
-})()
+
+	function generateNotify(message, type) {
+		$.notify({
+			title: "<strong>Geez</strong>",
+			message: message,
+		}, {
+			type: type,
+			animate: {
+				enter: 'animate__animated animate__fadeInLeft',
+				exit: 'animate__animated animate__fadeOutRight',
+			},
+			placement: {
+				from: "bottom",
+				align: "right"
+			},
+			offset: 20,
+			spacing: 10,
+			z_index: 1031,
+			newest_on_top: true,
+		})
+	}
+
+	function generateSuccessNotify() {
+		generateNotify("Successful copied!", 'success');
+	}
+
+	function generateFailedNotify() {
+		generateNotify("Copy failed!", 'danger');
+	}
+});
