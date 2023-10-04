9f2e8479c069e632b537252e3f58042cf799eb0f
adrien1018 <adrien1018@users.noreply.github.com>
2023-10-04 19:55:10 +0000

    Fix contest user links

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 module Tioj
-  VERSION = '3.0.1'
+  VERSION = '3.0.2'
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
