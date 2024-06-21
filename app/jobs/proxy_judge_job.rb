2d30a5beee513e24c341d5acb4b5c2ff24c2808d
OmeletWithoutEgg <2qbingxuan@gmail.com>
2024-06-21 09:45:11 +0800

    Update proxy judge job
    - Add different comment style for all languages
    - Return JE when exception
    - Try to add broadcasting

--- app/jobs/proxy_judge_job.rb
+++ app/jobs/proxy_judge_job.rb
@@ -6,43 +6,60 @@ class ProxyJudgeJob < ApplicationJob
   #   Rails.logger.info "args = #{args}"
   # end
 
-  # TODO add panel to check proxy judge active job status?
   def perform(submission, problem)
     code = submission.code_content.code
-    code << "\n// tioj-proxy - " << rand(8**36).to_s(36) # XXX: hack for exactly same code
-    # TODO this comment style only works for C/C++
 
-    # Rails.logger.info code
-    # Rails.logger.info "proxyjudge_type = #{problem.proxyjudge_type}"
-    # Rails.logger.info "proxyjudge_args = #{problem.proxyjudge_args}"
-    # Rails.logger.info submission.compiler
-    # return
-
-    case problem.proxyjudge_type.to_sym
-    when :codeforces
-      @proxy = Judges::CF.new()
-    when :poj
-      @proxy = Judges::POJ.new()
-    else
-      raise 'Unknown problem.proxyjudge_type'
-    end
-
-    @proxy.submit!(problem.proxyjudge_args, submission.compiler.name, code)
-
-    submission.result = "received"
-    submission.save
-
-    loop do
-      if @proxy.done?
-        @proxy.summary!
-        submission.result = @proxy.verdict
-        submission.total_time = @proxy.time
-        submission.total_memory = @proxy.memory
-        submission.save
-        # TODO broadcast to submissionchannel?
-        break
+    begin
+      case submission.compiler.format_type # XXX: hack for exactly same code
+      when "language-c", "language-cpp"
+        code << "\n// tioj-proxy - " << rand(8**36).to_s(36)
+      when "language-haskell"
+        code << "\n-- tioj-proxy - " << rand(8**36).to_s(36)
+      when "language-python"
+        code << "\n# tioj-proxy - " << rand(8**36).to_s(36)
       end
-      sleep 3
+
+      # Rails.logger.info code
+      # Rails.logger.info "proxyjudge_type = #{problem.proxyjudge_type}"
+      # Rails.logger.info "proxyjudge_args = #{problem.proxyjudge_args}"
+      # Rails.logger.info submission.compiler
+      # return
+
+      case problem.proxyjudge_type.to_sym
+      when :codeforces
+        @proxy = Judges::CF.new()
+      when :poj
+        @proxy = Judges::POJ.new()
+      else
+        raise 'Unknown problem.proxyjudge_type'
+      end
+
+      @proxy.submit!(problem.proxyjudge_args, submission.compiler.name, code)
+
+      submission.result = "received"
+      submission.save
+      # TODO check broadcast working?
+      ActionCable.server.broadcast("submission_#{submission.id}_overall",
+                                   {result: 'received', id: submission.id})
+
+      # TODO shall we set a timeout or max retry?
+      until @proxy.done? do
+        sleep 3
+      end
+      @proxy.summary!
+      submission.result = @proxy.verdict
+      submission.total_time = @proxy.time
+      submission.total_memory = @proxy.memory
+      submission.save
+      ActionCable.server.broadcast("submission_#{submission.id}_overall",
+         [:id, :score, :result, :total_time, :total_memory, :message].map{|attr|
+           [attr, submission.read_attribute(attr)]
+         }.to_h
+      )
+    rescue Exception => e
+      Rails.logger.error e
+      submission.result = "JE"
+      submission.save
     end
   end
 end
