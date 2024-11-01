86bac55c05c88d1f6a7e4eb835848123dd55749d
OmeletWithoutEgg <2qbingxuan@gmail.com>
2024-11-01 16:16:24 +0800

    Fix summary columns in dashboard

--- app/javascript/pages/contests/contest_ranklist_reorder.js
+++ app/javascript/pages/contests/contest_ranklist_reorder.js
@@ -86,7 +86,7 @@ function reorderTableInternal(data, timestamp, initUserState, cellText, rowSumma
       let first_ac = data.first_ac[String(prob_id)] === 'user_' + user_id;
       $('#cell_item_' + key).html(cellText(current, user_state, first_ac));
     }
-    compare_keys['row_user_' + user_id] = rowSummary(user_id, user_state);
+    compare_keys['row_user_' + user_id] = rowSummary('user_' + user_id, user_state);
   }
   for (const team_id of data.teams) {
     let team_state = {...initUserState};
@@ -97,7 +97,7 @@ function reorderTableInternal(data, timestamp, initUserState, cellText, rowSumma
       let first_ac = data.first_ac[String(prob_id)] === 'team_' + team_id;
       $('#cell_item_' + key).html(cellText(current, team_state, first_ac));
     }
-    compare_keys['row_team_' + team_id] = rowSummary(team_id, team_state);
+    compare_keys['row_team_' + team_id] = rowSummary('team_' + team_id, team_state);
   }
   console.log({ compare_keys });
   let tbody = $('#dashboard_table_body');
