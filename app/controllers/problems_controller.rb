2a24ab5e251e58bcc0b0d0cc3d534504781e9691
ckeisc43rd-cjtsai <jimtsai9596@gmail.com>
2025-07-26 00:19:20 +0800

    auto select current setter in update and create (issues in creating)

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -102,7 +102,7 @@ class ProblemsController < ApplicationController
 
   def create
     params[:problem][:compiler_ids] ||= []
-    @problem = Problem.new(check_params())
+    @problem = Problem.new(check_params(), setter: current_user)
     @ban_compiler_ids = params[:problem][:compiler_ids].map(&:to_i).to_set
     respond_to do |format|
       if @problem.save
