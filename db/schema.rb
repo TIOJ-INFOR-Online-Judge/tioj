a90ac4063667a9ec0d50869f69c96416e947c7f9
adrien1018 <adrien1018@users.noreply.github.com>
2026-07-06 00:11:01 +0000

    Rust migration for ban compilers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_12_053019) do
+ActiveRecord::Schema[7.2].define(version: 2026_07_05_234638) do
   create_table "active_admin_comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "namespace"
     t.text "body", size: :medium
